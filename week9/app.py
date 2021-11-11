9b69d2c719507883c1bdbd9341748b76ecb110ed
Jack Boettcher <jcboettcher@college.harvard.edu>
2021-11-11 00:36:28 -0500

    week 9

---
+++ week9/app.py
@@ -0,0 +1,43 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# request.method -> which method are you trying to access?
+# request.form.get -> get a specific value from a form
+# db.execute -> execute an SQL query on database db
+# redirect -> redirect to a specific route
+# render_template -> render an html file possibly with additional dynamic arguments
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        print(name,month,day)
+
+        # INSERT INTO
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)",name,month,day)
+
+        return redirect("/")
+
+    else:
+
+        # TODO: Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM birthdays")
+
+        return render_template("index.html", birthdays=birthdays)
+
+
